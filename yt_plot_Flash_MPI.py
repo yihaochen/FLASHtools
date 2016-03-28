fa1582c45d1f68b20403f736ea3731c7373a2150
Yi-Hao Chen <ychen@astro.wisc.edu>
2016-03-28 13:04:52 -0500

    Restructure plot script with separate taskpull

--- yt_plot_Flash_MPI.py
+++ yt_plot_Flash_MPI.py
@@ -1,46 +1,25 @@
 #!/usr/bin/env python
-import time
-t0 = time.time()
 import matplotlib
 matplotlib.use('Agg')
 import yt
 import os
 import sys
-sys.path.append(os.getenv('HOME') + '/lib/util')
+#sys.path.append(os.getenv('HOME') + '/lib/util')
 import util
-#from mpi_taskpull import taskpull
+import MPI_taskpull2
 import logging
 logging.getLogger('yt').setLevel(logging.ERROR)
-from mpi4py import MPI
 import matplotlib.pyplot as plt
 import numpy as np
 
-from tools import calcNozzleCoords, read_par
+from tools import calcNozzleCoords
 from plotSlices import plotSliceField
 from plotProjections import plotProjectionField
 
-def enum(*sequential, **named):
-    """Handy way to fake an enumerated type in Python
-    http://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python
-    """
-    enums = dict(zip(sequential, range(len(sequential))), **named)
-    return type('Enum', (), enums)
-
-# Define MPI message tags
-tags = enum('READY', 'DONE', 'EXIT', 'START')
-
-# Initializations and preliminaries
-comm = MPI.COMM_WORLD   # get MPI communicator object
-size = comm.size        # total number of processes
-rank = comm.rank        # rank of this process
-status = MPI.Status()   # get MPI status object
-name=MPI.Get_processor_name()
 dir = './'
 regex = 'MHD_Jet*_hdf5_plt_cnt_[0-9][0-9][0-9][0-9]'
-#regex = 'MHD_Jet_10Myr_hdf5_plt_cnt_[0-9][0-9][0-9][0-9]'
-#regex = 'MHD_Jet_hdf5_chk_001[0-9]'
 files = None
-zoom_facs = [8,16,128]
+zoom_facs = [8]
 proj_axes= ['x']
 
 
@@ -48,25 +27,23 @@ proj_axes= ['x']
 fields_velocity = ['velocity_z']
 fields_part = None
 fields = ['density', 'pressure', 'temperature', 'velocity_y', 'velocity_z', 'jet ',\
-          'magnetic_field_x', 'magnetic_field_y', 'magnetic_field_z', 'magnetic_pressure']
+          'magnetic_field_x', 'magnetic_field_y', 'magnetic_field_z', 'magnetic_pressure',\
+          'particle_gamc', 'plasma_beta']
 #fields = ['density', 'pressure','shok', 'velocity_y', 'velocity_z', 'jet ']
 #fields = ['density', 'pressure', 'temperature', 'velocity_magnitude', 'velocity_z']
 #fields = ['magnetic_pressure']
-#fields = ['density', 'pressure', 'temperature', 'velocity_y', 'velocity_z']
-#fields = ['pressure']
-#fields = ['magnetic_field_z']
-#fields = ['velocity_y']
-#fields = ['shok']
+#fields = ['density', 'temperature']
 #fields = ['plasma_beta']
-#fields = ['particle_gamc']
+#fields = ['magnetic_field_x', 'magnetic_field_y', 'magnetic_field_z', 'magnetic_pressure',\
+#          'velocity_y', 'velocity_z']
 
 
 def rescan(printlist=False):
     files = util.scan_files(dir, regex=regex, walk=True, printlist=printlist, reverse=False)
     return files
 
+
 def worker_fn(file, field, proj_axis, zoom_fac):
-    t_worker_0 = time.time()
     particle_path = file.fullpath.replace('plt_cnt', 'part')\
                     if 'plt_cnt' in file.fullpath\
                     else file.fullpath.replace('chk', 'part')
@@ -83,7 +60,7 @@ def worker_fn(file, field, proj_axis, zoom_fac):
     center = (0.0,0.0,7.714E22) if proj_axis=='z' else (0.0,0.0,0.0)
     #center = (6.125E+20,  1.414E+20, -8.953E+20) if zoom_fac==128 else (0.0,0.0,0.0)
     fields_grid = ['density', 'pressure', 'velocity_y', 'velocity_z'] if zoom_fac >=16 \
-             else ['velocity_z']
+             else ['velocity_z', 'temperature', 'density']
     #fields_grid = True
     figuredir = 'figures_%s' % proj_axis if proj_axis != 'x' else 'figures'
     figuredir = figuredir + '_zoom%i' % zoom_fac
@@ -118,34 +95,34 @@ def worker_fn(file, field, proj_axis, zoom_fac):
 
 
     else:
+        dirnamesplit = os.path.abspath(file.pathname).split('_')
+        if dirnamesplit[-1] in ['h1','hinf', 'h0']:
+            sim_name = dirnamesplit[-1]
+        else:
+            sim_name = dirnamesplit[-2] + '_' + dirnamesplit[-1]
         plotSliceField(ds, zoom_fac=zoom_fac, center=center, proj_axis=proj_axis, field=field,\
                    plotgrid=fields_grid, plotvelocity=fields_velocity, nozzleCoords=nozzleCoords, \
-                   annotate_particles=fields_part,annotate_part_info=False,\
-                   savepath=os.path.join(dir,figuredir,field))
-    t_worker_1 = time.time()
-    elapsed_time = '%5.1f s' % (t_worker_1-t_worker_0)
-    return ds.basename[-4: ], '%18s' % field, elapsed_time
+                   annotate_particles=fields_part,annotate_part_info=False, sim_name=sim_name,\
+                   savepath=os.path.join(dir,figuredir,field.strip()))
+    return ds.basename[-4: ], field
 
 
-
-
-def worker_test(file, field):
+def worker_test(file, field, proj_axis, zoom_fac):
     ds = yt.load(file.fullpath)
-    return (file.filename, field)
+    return file.filename, field, proj_axis, zoom_fac
 
-def tasks_gen(files, fields, proj_axes, zoom_facs):
+
+def tasks_gen(i0, fields, proj_axes, zoom_facs):
     #sys.stdout.write("Plotting %s... \n" % (file.filename))
+    files = rescan(False)[i0:]
     for zoom_fac in zoom_facs:
         for proj_axis in proj_axes:
             for file in files:
                 for field in fields:
-                    yield (file, field, proj_axis, zoom_fac)
+                    yield file, field, proj_axis, zoom_fac
 
 
-if rank == 0:
-    i0 = int(sys.argv[1]) if len(sys.argv) > 1 else 0
-    files = rescan(True)[i0:]
-    tasks = tasks_gen(files, fields, proj_axes, zoom_facs)
+def init():
     for zoom_fac in zoom_facs:
         for proj_axis in proj_axes:
             figuredir = 'figures_%s' % proj_axis if proj_axis != 'x' else 'figures'
@@ -153,59 +130,11 @@ if rank == 0:
             if not os.path.exists(os.path.join(dir,figuredir)):
                 os.mkdir(os.path.join(dir, figuredir))
             for field in fields:
-                if not os.path.exists(os.path.join(dir,figuredir,field)):
-                    os.mkdir(os.path.join(dir,figuredir,field))
-    t1 = time.time()
-    sys.stdout.write("Timer started\n")
+                if not os.path.exists(os.path.join(dir,figuredir,field.strip())):
+                    os.mkdir(os.path.join(dir,figuredir,field.strip()))
 
-    # Master process executes code below
-    task_index = 0
-    num_workers = size - 1
-    closed_workers = 0
-    sys.stdout.write("Master starting with %d workers\n" % num_workers)
-    while closed_workers < num_workers:
-        data = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
-        source = status.Get_source()
-        tag = status.Get_tag()
-        if tag == tags.READY:
-            # Worker is ready, so send it a task
-            try:
-                comm.send(tasks.next(), dest=source, tag=tags.START)
-                #print("Sending task to worker %03d" % (source))
-            except StopIteration:
-                comm.send(None, dest=source, tag=tags.EXIT)
-            #if task_index < len(tasks):
-            #    comm.send(tasks[task_index], dest=source, tag=tags.START)
-            #    print("Sending task %d to worker %d" % (task_index, source))
-            #    task_index += 1
-            #else:
-            #    comm.send(None, dest=source, tag=tags.EXIT)
-        elif tag == tags.DONE:
-            results = data
-            sys.stdout.write("Worker %03d returned data: %s\n" % (source, str(data)))
-        elif tag == tags.EXIT:
-            sys.stdout.write("Worker %03d exited. (%3d/%3d)\n" % (source, num_workers-closed_workers-1, num_workers))
-            closed_workers += 1
+i0 = int(sys.argv[1]) if len(sys.argv) > 1 else 0
+tasks = tasks_gen(i0, fields, proj_axes, zoom_facs)
 
-    print("Master finishing")
-    t2 = time.time()
-    print 'Total time: %.2f s\n--\ninitialization: %.2f s\nploting: %.2f'\
-           %  (t2-t0, t1-t0, t2-t1)
-else:
-    # Worker processes execute code below
-    name = MPI.Get_processor_name()
-    #sys.stdout.write("I am a worker with rank %03d on %s.\n" % (rank, name))
-    while True:
-        comm.send(None, dest=0, tag=tags.READY)
-        task = comm.recv(source=0, tag=MPI.ANY_TAG, status=status)
-        tag = status.Get_tag()
-
-        if tag == tags.START:
-            #sys.stdout.write("Worker %03d on %s got a job.\n" % (rank, name))
-            # Do the work here
-            result = worker_fn(*task)
-            comm.send(result, dest=0, tag=tags.DONE)
-        elif tag == tags.EXIT:
-            break
-
-    comm.send(None, dest=0, tag=tags.EXIT)
+results = MPI_taskpull2.taskpull(worker_fn, tasks, initialize=init, print_result=True)
+#print results
