655423117a945b409a5753733887360ad8fc28e1
Yi-Hao Chen <ychen@astro.wisc.edu>
2016-12-03 21:44:40 -0600

    Multiple directoris, improved particle plots

--- yt_plot_Flash_MPI.py
+++ yt_plot_Flash_MPI.py
@@ -16,27 +16,38 @@ from tools import calcNozzleCoords, calcDen0
 from plotSlices import plotSliceField
 from plotProjections import plotProjectionField
 
-dir = '/d/d9/ychen/FLASH4/stampede/0529_L45_M10_b1_h1/'
-regex = 'MHD_Jet*_hdf5_plt_cnt_0[2-9][0,2,4,6,8]0'
-#regex = 'MHD_Jet*_hdf5_plt_cnt_[0-9][0-9][0-9][0-9]'
+#dirs = ['/home/ychen/data/0only_0314_h1_nojiggle',\
+#        '/home/ychen/data/0only_0330_h0_nojiggle',\
+#         '/home/ychen/data/0only_0525_hinf_nojiggle']
+#dirs = ['/home/ychen/data/0only_0518_hydro_nojiggle']
+#dirs = ['/home/ychen/data/0only_1022_h1_10Myr']
+#dirs = ['/home/ychen/data/0only_0204_hinf_10Myr',\
+#        '/home/ychen/data/0only_0204_h0_10Myr']
+#dir = '/d/d9/ychen/FLASH4/stampede/0529_L45_M10_b1_h1/'
+dirs = ['./']
+#regex = 'MHD_Jet*_hdf5_plt_cnt_0[2-9][0,2,4,6,8]0'
+regex = 'MHD_Jet*_hdf5_plt_cnt_[0-9][0-9][0-9][0-9]'
 files = None
-zoom_facs = [10]
+zoom_facs = [1]
 proj_axes= ['x']
-figuredirtemplate = 'figures%s_zoom%i_shok'
+figuredirtemplate = 'figures%s_zoom%i'
 
 
 #annotate_particles = True if zoom_fac >= 2 else False
-fields_velocity = ['velocity_z']
-fields_part = None
-#fields = ['density', 'pressure', 'temperature', 'velocity_y', 'velocity_z', 'jet ',\
-#          'magnetic_field_x', 'magnetic_field_y', 'magnetic_field_z', 'magnetic_pressure',\
-#           'particle_gamc', 'plasma_beta']
-fields = ['particle_nuc', 'particle_gamc', 'particle_age', 'particle_magp']
-#fields = ['particle_magp']
+fields_velocity = None
+fields_part = ['density']
+fields = ['density', 'pressure', 'temperature', 'velocity_y', 'velocity_z', 'jet ',\
+          'magnetic_field_x', 'magnetic_field_z', 'magnetic_pressure',\
+           'particle_gamc', 'plasma_beta', 'entropy']
+#fields = ['density', 'pressure', 'temperature', 'velocity_x', 'velocity_z', 'jet ',\
+#          'entropy']
+#fields = ['particle_nuc', 'particle_gamc', 'particle_age', 'particle_magp']
+fields = ['particle_gamc']
+#fields = ['plasma_beta']
 
 
-def rescan(printlist=False):
-    files = util.scan_files(dir, regex=regex, walk=True, printlist=printlist, reverse=False)
+def rescan(dir, printlist=False):
+    files = util.scan_files(dir, regex=regex, walk=False, printlist=printlist, reverse=False)
     return files
 
 e  = yt.utilities.physical_constants.elementary_charge #4.803E-10 esu
@@ -44,17 +55,18 @@ me = yt.utilities.physical_constants.mass_electron #9.109E-28
 c  = yt.utilities.physical_constants.speed_of_light #2.998E10
 
 def worker_fn(file, field, proj_axis, zoom_fac):
-    particle_path = file.fullpath.replace('plt_cnt', 'part')\
-                    if 'plt_cnt' in file.fullpath\
-                    else file.fullpath.replace('chk', 'part')
-    if os.path.exists(particle_path):
-        ds = yt.load(file.fullpath, particle_filename=particle_path)
-    else:
-        ds = yt.load(file.fullpath)
-        global fields_part
-        fields_part = False
+    #particle_path = file.fullpath.replace('plt_cnt', 'part')\
+    #                if 'plt_cnt' in file.fullpath\
+    #                else file.fullpath.replace('chk', 'part')
+    #if os.path.exists(particle_path):
+    #    ds = yt.load(file.fullpath, particle_filename=particle_path)
+    #else:
+    #    ds = yt.load(file.fullpath)
+    #    global fields_part
+    #    fields_part = False
+    ds = yt.load(file.fullpath)
 
-    nozzleCoords = calcNozzleCoords(ds, proj_axis)
+    #nozzleCoords = calcNozzleCoords(ds, proj_axis)
     #nozzleCoords = None if proj_axis=='z' or zoom_fac<4 else nozzleCoords
     nozzleCoords = None
     center = (0.0,0.0,7.714E22) if proj_axis=='z' else (0.0,0.0,0.0)
@@ -69,11 +81,21 @@ def worker_fn(file, field, proj_axis, zoom_fac):
     #               plotgrid=fields_grid, plotvelocity=fields_velocity, nozzleCoords=nozzleCoords, \
     #               annotate_particles=fields_part,annotate_part_info=False,\
     #               savepath=os.path.join(dir,figuredir,field))
+    dirnamesplit = os.path.abspath(file.pathname).split('_')
+    if dirnamesplit[-1] in ['h1','hinf', 'h0'] and dirnamesplit[-2] in ['b1']:
+        sim_name = dirnamesplit[-1]
+    else:
+        sim_name = dirnamesplit[-2] + '_' + dirnamesplit[-1]
+
     if field in ['particle_gamc', 'particle_age', 'particle_nuc', 'particle_magp']:
         ad = ds.all_data()
         #den0 = calcDen0(ad['all', 'particle_tadd'])
-        jetfil = np.logical_and((ad['all', 'particle_tag'] % 1 == 0),\
-                                (ad['all', 'particle_shok']==1))
+        if ('all', 'particle_type') in ds.derived_field_list:
+            jetfil = np.logical_and((ad['all', 'particle_type'] == 1.0),\
+                                    (ad['all', 'particle_shok'] <= 0.01))
+        else:
+            jetfil = (ad['all', 'particle_shok'] <= 0.01)
+        jetfil = np.logical_and(jetfil,ad['all', 'particle_tag'] % 10 == 0)
         #corefil = np.logical_and((ad['all', 'particle_den0']>den0-5E-31),\
         #                        (ad['all', 'particle_den0']<den0+5E-31))
         #initfil = ad['all', 'particle_tadd'] < 1.58E13
@@ -111,17 +133,20 @@ def worker_fn(file, field, proj_axis, zoom_fac):
             xaxis = ad['all', 'particle_position_y'][filter]/3.08567758E21
             yaxis = ad['all', 'particle_position_z'][filter]/3.08567758E21
             fig=plt.figure(figsize=(8,12))
-            xlim=30; ylim=60
+            xlim=ds.domain_width[1].in_units('kpc')/zoom_fac/2.0
+            ylim=ds.domain_width[2].in_units('kpc')/zoom_fac/2.0
         elif proj_axis == 'y':
             xaxis = ad['all', 'particle_position_z'][filter]/3.08567758E21
             yaxis = ad['all', 'particle_position_x'][filter]/3.08567758E21
             fig=plt.figure(figsize=(12,6))
-            xlim=60; ylim=30
+            xlim=ds.domain_width[2].in_units('kpc')/zoom_fac/2.0
+            ylim=ds.domain_width[0].in_units('kpc')/zoom_fac/2.0
         elif proj_axis == 'z':
             xaxis = ad['all', 'particle_position_x'][filter]/3.08567758E21
             yaxis = ad['all', 'particle_position_y'][filter]/3.08567758E21
             fig=plt.figure(figsize=(8,7))
-            xlim=5; ylim=5
+            xlim=ds.domain_width[0].in_units('kpc')/zoom_fac/2.0
+            ylim=ds.domain_width[1].in_units('kpc')/zoom_fac/2.0
 
         #plt.hist(gamc, bins=50)
         ax=fig.add_subplot(111)
@@ -132,6 +157,8 @@ def worker_fn(file, field, proj_axis, zoom_fac):
         ax.annotate('%6.3f Myr' % (float(ds.current_time)/3.15569E13),\
                     (1,0), xytext=(0.05, 0.96),  textcoords='axes fraction',\
                     horizontalalignment='left', verticalalignment='center')
+        ax.annotate(sim_name, (1,0), xytext=(0.85, 0.96), textcoords='axes fraction',\
+                    horizontalalignment='left', verticalalignment='center')
 
         sc=ax.scatter(xaxis,yaxis,s=1,c=fdata,linewidth=0,cmap=cmap,vmin=vmin,vmax=vmax,alpha=0.8)
         try:
@@ -140,20 +167,14 @@ def worker_fn(file, field, proj_axis, zoom_fac):
         except:
             pass
         plt.tight_layout()
-        plt.savefig(os.path.join(dir,figuredir,field,file.filename), dpi=150)
-
+        plt.savefig(os.path.join(file.pathname,figuredir,field,file.filename), dpi=150)
 
     else:
-        dirnamesplit = os.path.abspath(file.pathname).split('_')
-        if dirnamesplit[-1] in ['h1','hinf', 'h0']:
-            sim_name = dirnamesplit[-1]
-        else:
-            sim_name = dirnamesplit[-2] + '_' + dirnamesplit[-1]
         plotSliceField(ds, zoom_fac=zoom_fac, center=center, proj_axis=proj_axis, field=field,\
-                   plotgrid=fields_grid, plotvelocity=fields_velocity, nozzleCoords=nozzleCoords, \
+                   plotgrid=fields_grid, plotvelocity=fields_velocity, \
                    annotate_particles=fields_part,annotate_part_info=False, sim_name=sim_name,\
-                   savepath=os.path.join(dir,figuredir,field.strip()))
-    return ds.basename[-4: ], field
+                   savepath=os.path.join(file.pathname,figuredir,field.strip()))
+    return sim_name, ds.basename[-4: ], field
 
 
 def worker_test(file, field, proj_axis, zoom_fac):
@@ -161,29 +182,31 @@ def worker_test(file, field, proj_axis, zoom_fac):
     return file.filename, field, proj_axis, zoom_fac
 
 
-def tasks_gen(i0, fields, proj_axes, zoom_facs):
+def tasks_gen(dirs, i0, fields, proj_axes, zoom_facs):
     #sys.stdout.write("Plotting %s... \n" % (file.filename))
-    files = rescan(False)[i0:]
-    for zoom_fac in zoom_facs:
-        for proj_axis in proj_axes:
-            for file in files:
-                for field in fields:
-                    yield file, field, proj_axis, zoom_fac
+    for dir in dirs:
+        files = rescan(dir, False)[i0:]
+        for zoom_fac in zoom_facs:
+            for proj_axis in proj_axes:
+                for file in files:
+                    for field in fields:
+                        yield file, field, proj_axis, zoom_fac
 
 
 def init():
-    for zoom_fac in zoom_facs:
-        for proj_axis in proj_axes:
-            axis = '_%s' % proj_axis if proj_axis != 'x' else ''
-            figuredir = figuredirtemplate % (axis, zoom_fac)
-            if not os.path.exists(os.path.join(dir,figuredir)):
-                os.mkdir(os.path.join(dir, figuredir))
-            for field in fields:
-                if not os.path.exists(os.path.join(dir,figuredir,field.strip())):
-                    os.mkdir(os.path.join(dir,figuredir,field.strip()))
+    for dir in dirs:
+        for zoom_fac in zoom_facs:
+            for proj_axis in proj_axes:
+                axis = '_%s' % proj_axis if proj_axis != 'x' else ''
+                figuredir = figuredirtemplate % (axis, zoom_fac)
+                if not os.path.exists(os.path.join(dir,figuredir)):
+                    os.mkdir(os.path.join(dir, figuredir))
+                for field in fields:
+                    if not os.path.exists(os.path.join(dir,figuredir,field.strip())):
+                        os.mkdir(os.path.join(dir,figuredir,field.strip()))
 
 i0 = int(sys.argv[1]) if len(sys.argv) > 1 else 0
-tasks = tasks_gen(i0, fields, proj_axes, zoom_facs)
+tasks = tasks_gen(dirs, i0, fields, proj_axes, zoom_facs)
 
 results = MPI_taskpull2.taskpull(worker_fn, tasks, initialize=init, print_result=True)
 #print results
