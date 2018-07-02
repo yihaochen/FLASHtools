840b95e831536b34d765884f1782ba541e4dc048
Yi-Hao Chen <ychen@astro.wisc.edu>
2018-07-02 11:41:57 -0500

    Add Spitzer conduction fields

--- conduction/yt_conduction_fields.py
+++ conduction/yt_conduction_fields.py
@@ -1,6 +1,9 @@
 import yt
+import numpy as np
 from yt.fields.derived_field import ValidateSpatial
 from yt.funcs import just_one
+
+jet_thresh = 1E-3
 
 @yt.derived_field('xray_cooling_time', sampling_type='cell', units='Myr')
 def _xray_cooling_time(field, data):
@@ -16,13 +19,15 @@ def _total_heating_rate_density(field, data):
     xray_emis = ('gas', 'xray_emissivity_0.1_100_keV')
     if xray_emis not in data.ds.derived_field_list:
         xray_fields = yt.add_xray_emissivity_field(data.ds, 0.1, 100, table_type='apec', metallicity=0.5)
-    return - data['spitzer_heat_flux_divergence'] - data[xray_emis]
+    return -data['spitzer_heat_flux_divergence'] - data[xray_emis]
 
 @yt.derived_field('spitzer_conduction_coefficient', sampling_type='cell', units='cm**2/s')
 def _spitzer_conduction_coefficient(field, data):
     T1 = data['temperature'].to_equivalent('keV', 'thermal')/10/yt.units.keV
     n = data['H_nuclei_density']/1E-3/yt.units.cm**(-3)
-    return 4E32*T1**2.5/n*yt.units.cm**2*yt.units.s**(-1)
+    jetregion = data['jet '] > jet_thresh
+    T1[jetregion] = T1[jetregion]/1E6
+    return 0.1*4E32*T1**2.5/n*yt.units.cm**2*yt.units.s**(-1)
 
 basename = 'spitzer_heat_flux'
 xn = basename + '_x'
