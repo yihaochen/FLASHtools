99c52a9e6934059a215a9c90ed01673182fe6936
Yi-Hao Chen <ychen@astro.wisc.edu>
2018-08-22 15:28:05 -0500

    Use full Spitzer conduction rate; correct sign of total_heating_rate

--- conduction/yt_conduction_fields.py
+++ conduction/yt_conduction_fields.py
@@ -14,20 +14,15 @@ def _xray_cooling_time(field, data):
     thermal_energy = 1./(gamma-1)*data['pressure']
     return thermal_energy/data[xray_emis]
 
-@yt.derived_field('total_heating_rate_density', sampling_type='cell', units='erg/s/cm**3')
-def _total_heating_rate_density(field, data):
-    xray_emis = ('gas', 'xray_emissivity_0.1_100_keV')
-    if xray_emis not in data.ds.derived_field_list:
-        xray_fields = yt.add_xray_emissivity_field(data.ds, 0.1, 100, table_type='apec', metallicity=0.5)
-    return -data['spitzer_heat_flux_divergence'] - data[xray_emis]
-
 @yt.derived_field('spitzer_conduction_coefficient', sampling_type='cell', units='cm**2/s')
 def _spitzer_conduction_coefficient(field, data):
     T1 = data['temperature'].to_equivalent('keV', 'thermal')/10/yt.units.keV
     n = data['H_nuclei_density']/1E-3/yt.units.cm**(-3)
+
+    # Suppress the coefficient in the hot jet region
     jetregion = data['jet '] > jet_thresh
     T1[jetregion] = T1[jetregion]/1E6
-    return 0.1*4E32*T1**2.5/n*yt.units.cm**2*yt.units.s**(-1)
+    return 4E32*T1**2.5/n*yt.units.cm**2*yt.units.s**(-1)
 
 basename = 'spitzer_heat_flux'
 xn = basename + '_x'
@@ -73,9 +68,17 @@ def _spitzer_heating_rate(field, data):
     return -data[('gas', 'spitzer_heat_flux_divergence')]*data[('gas', 'cell_volume')]
 
 
+@yt.derived_field('total_heating_rate_density', sampling_type='cell', units='erg/s/cm**3')
+def _total_heating_rate_density(field, data):
+    xray_emis = ('gas', 'xray_emissivity_0.1_100_keV')
+    if xray_emis not in data.ds.derived_field_list:
+        xray_fields = yt.add_xray_emissivity_field(data.ds, 0.1, 100, table_type='apec', metallicity=0.5)
+    return -data['spitzer_heat_flux_divergence'] - data[xray_emis]
+
+
 @yt.derived_field(('gas', 'total_heating_rate'), sampling_type="cell", units='erg/s')
 def _total_heating_rate(field, data):
-    return -data[('gas', 'total_heating_rate_density')]*data[('gas', 'cell_volume')]
+    return data[('gas', 'total_heating_rate_density')]*data[('gas', 'cell_volume')]
 
 
 @yt.derived_field(('gas', 'total_cooling_time'), sampling_type="cell", units='Myr')
